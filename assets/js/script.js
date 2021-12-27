e525ec469d718431c72422ac2a03ade41bdce9b6
Shibam Biswas <shivambiswas87@gmail.com>
2021-12-27 20:44:36 +0530

    all notes and my notes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,12 @@
-document.querySelector('.custom-file-input').addEventListener('change',function(e){
-    var fileName = document.getElementById("file").files[0].name;
-    var nextSibling = e.target.nextElementSibling
-    nextSibling.innerText = fileName
-});
+var customFileInput = document.querySelector('.custom-file-input') !== null;
+if (customFileInput) {
+
+    customFileInput.addEventListener('change', function (e) {
+        var fileName = document.getElementById("file").files[0].name;
+        var nextSibling = e.target.nextElementSibling
+        nextSibling.innerText = fileName
+    });
+}
 $(document).ready(function(){
 
 
--- screens/layouts/footer.php
+++ screens/layouts/footer.php
@@ -102,37 +102,7 @@
 </div>
 
 
-
-<!-- Optional JavaScript -->
-<!-- jQuery first, then Popper.js, then Bootstrap JS -->
-<script src="https://code.jquery.com/jquery-3.3.1.min.js" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-
-<?php
-global $cssArr;
-if(!empty($cssArr)){
-    foreach($cssArr as $cs){
-        ?>
-        <link rel="stylesheet" href="<?php echo $cs;?>">
-        <?php
-    }
-}
-global $jsArr;
-if(!empty($jsArr)){
-    foreach($jsArr as $js){
-        ?>
-        <script src="<?php echo $js;?>" crossorigin="anonymous"></script>
-<?php
-    }
-}
-
-?>
-<script src="<?php echo \utils\Url::getAssetUrl('js/script.js?v=1');?>"></script>
-<?php if(\utils\Url::isDashboard()){?>
-<script src="<?php echo \utils\Url::getAssetUrl('js/admin.js?v=1');?>"></script>
-
-<?php } ?>
+<?php include BASE_PATH . '/screens/layouts/_footer.php';?>
 
 
 </body>
